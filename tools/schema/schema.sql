586b23b9a69f6037c37fa30dd17a03eec2d0db0d
lavalink <162498817+1deals@users.noreply.github.com>
2024-08-09 12:32:14 +0100

    Update schema.sql

--- tools/schema/schema.sql
+++ tools/schema/schema.sql
@@ -21,7 +21,8 @@ CREATE TABLE IF NOT EXISTS voicemaster.channels (
 CREATE TABLE IF NOT EXISTS forcenick (
     guild_id BIGINT, 
     user_id BIGINT, 
-    name TEXT
+    name TEXT,
+    PRIMARY KEY (guild_id, user_id)
 );
 
 CREATE TABLE IF NOT EXISTS welcome (
