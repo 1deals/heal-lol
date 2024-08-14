b82c808af4e1121c0f7be5a787c2989dcb767f3b
1deals <1dealsdiscord@gmail.com>
2024-08-14 11:02:35 +0100

    he

--- cogs/lastfm.py
+++ cogs/lastfm.py
@@ -135,7 +135,7 @@ class LastFM(Cog):
         data = await self.bot.pool.fetchrow("SELECT command FROM lastfm WHERE user_id = $1", message.author.id)
         
         if data:
-            alias = data['alias_name'].strip().lower()
+            alias = data['command'].strip().lower()
             if message.content.strip().lower() == alias:
                 ctx = await self.bot.get_context(message)
                 await ctx.invoke(self.bot.get_command('lf np'))
