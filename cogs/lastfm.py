f1b1ba6208dbecbf95ab11c5128118d8aa514a4d
1deals <1dealsdiscord@gmail.com>
2024-08-14 10:50:30 +0100

    ya

--- cogs/lastfm.py
+++ cogs/lastfm.py
@@ -77,46 +77,49 @@ class LastFM(Cog):
         aliases = ["np"]
     )
     @commands.cooldown(1, 5, commands.BucketType.user)
-    async def lastfm_nowplaying(self, ctx: Context, *, user: Union[discord.Member, discord.User]= None):
+    async def lastfm_nowplaying(self, ctx: Context, *, user: Union[discord.Member, discord.User] = None):
         if user is None:
             user = ctx.author
+        
         await ctx.typing()
-        data = await self.bot.pool.fetchrow("SELECT * FROM lastfm WHERE user_id = $1", user.id)
-        if not data:
-            return await ctx.lastfm(f"**{user.name}** hasn't got their LastFM account linked.")
 
-        lastfm_username = data["lfuser"]
+        alias_data = await self.bot.pool.fetchrow("SELECT lfuser FROM command WHERE user_id = $1 AND command = $2", ctx.author.id, user.name)
+        
+        if alias_data:
+            lastfm_username = alias_data['lastfm_username']
+        else:
+            data = await self.bot.pool.fetchrow("SELECT * FROM lastfm WHERE user_id = $1", user.id)
+            if not data:
+                return await ctx.lastfm(f"**{user.name}** hasn't got their LastFM account linked.")
+            lastfm_username = data["lfuser"]
+
         async with aiohttp.ClientSession() as session:
             params = {
                 'method': 'user.getRecentTracks',
                 'user': lastfm_username,
-                'api_key': "bc8082588489f949216859abba6e52be",
+                'api_key': "bc8082588489f949216859abba6e52be", 
                 'format': 'json',
                 'limit': 1
             }
             async with session.get('http://ws.audioscrobbler.com/2.0/', params=params) as response:
                 if response.status != 200:
-                    return await ctx.deny("Failed to connect to LastFM API.")
+                    return await ctx.send("Failed to connect to LastFM API.")
 
                 data = await response.json()
                 if 'recenttracks' not in data or 'track' not in data['recenttracks']:
-                    return await ctx.warn(f"Could not retrieve data for user: {lastfm_username}")
+                    return await ctx.send(f"Could not retrieve data for user: {lastfm_username}")
 
                 track_info = data['recenttracks']['track'][0]
-
                 now_playing = track_info.get('@attr', {}).get('nowplaying') == 'true'
-
                 track_name = track_info['name']
                 artist_name = track_info['artist']['#text']
                 album_name = track_info.get('album', {}).get('#text', 'Unknown Album')
                 track_url = track_info['url']
-                album_art = track_info['image'][-1]['#text']
+                album_art = track_info['image'][-1]['#text'] if track_info['image'] else None
 
-                embed = discord.Embed(
-                    color=Colors.BASE_COLOR
-                )
-                embed.add_field(name = "**Track**", value = f"[{track_name}]({track_url})", inline  = True)
-                embed.add_field(name = "**Artist**", value = f"{artist_name}", inline = False)
+                embed = discord.Embed(color=Colors.BASE_COLOR)
+                embed.add_field(name="**Track**", value=f"[{track_name}]({track_url})", inline=True)
+                embed.add_field(name="**Artist**", value=artist_name, inline=False)
                 embed.set_author(name=f"{lastfm_username}")
                 if album_art:
                     embed.set_thumbnail(url=album_art)
@@ -150,6 +153,37 @@ class LastFM(Cog):
         
         await self.bot.pool.execute("DELETE FROM lastfm WHERE user_id = $1", ctx.author.id)
         return await ctx.lastfm(f"**Unlinked** your LastFM account successfully")
+    
+    @lastfm.command(
+        name = "variables",
+        aliases = ["vars"],
+        description = "Get a list of LastFM variables to use in you custom embeds."
+    )
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def lastfm_variables(self, ctx: Context):
+        return await ctx.neutral("{track_name} - Gets the name of the track \n{track_url} - Gets the url of the track\n{album_name} - Gets the name of the album \n{artist_name} - Gets the name of the artist(s) \n{album_art} - Gets the album art.")
+    
+    @lastfm.group(
+        name = "customcommand",
+        aliases = ["cc"],
+        description = "Configure your custom NowPlaying alias."
+    )
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def lastfm_customcommand(self, ctx: Context):
+        return await ctx.send_help(ctx.command)
+    
+    @lastfm_customcommand.command(
+        name = "set",
+        aliases = ["add"],
+        description = "Set your customcommand"
+    )
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def lastfm_customcommand_set(self, ctx: Context, *, customcommand: str = None):
+        if customcommand is None:
+            return await ctx.send_help(ctx.command)
+        
+        await self.bot.pool.execute("INSERT INTO lastfm (user_id, command) VALUES ($1, $2) ON CONFLICT (user_id) DO UPDATE SET command = $1", ctx.author.id ,customcommand)
+        return await ctx.lastfm(f"Set your **custom command** to **`{customcommand}`**")
 
 
 async def setup(bot: Heal):
