dcd05cebce1a9a41be6e45bc6820f218bfd7ffba
1deals <1dealsdiscord@gmail.com>
2024-08-15 12:50:59 +0000

    hm

--- main.py
+++ main.py
@@ -4,14 +4,6 @@ import datetime
 
 dotenv.load_dotenv()
 
-def get_bot_metrics(bot: Heal):
-    start_time = datetime.datetime.utcnow()
-    latency = bot.latency * 1000  
-    user_count = len(bot.users)
-    server_count = len(bot.guilds)
-    uptime = datetime.datetime.utcnow() - start_time
-    return latency, user_count, server_count, uptime
-
 if __name__ == "__main__":
     bot = Heal()
     bot.run('MTIzMjc3ODUwODQyNjgwOTM2NQ.GZClxX.tqhEsOYM7jNDfwEOXMQChAtw9OYYdSkF1zOGuU')
