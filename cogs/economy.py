5e400a3618de41b8c146ca434791ce8ae05e0dc3
1deals <1dealsdiscord@gmail.com>
2024-08-16 11:16:10 +0100

    dasewfwe

--- cogs/economy.py
+++ cogs/economy.py
@@ -17,13 +17,13 @@ class Economy(commands.Cog):
     def __init__(self, bot: Heal):
         self.bot = bot
 
-    @command(
-        name = "balace",
-        aliases = ["bal"],
-        description = "Get the balance of a user."
+    @commands.command(
+        name="balance",
+        aliases=["bal"],
+        description="Get the balance of a user."
     )
-    @cooldown(1, 5, commands.BucketType.user)
-    async def balance(self, ctx: Context, * , user: Union[discord.Member, discord.User] = None):
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def balance(self, ctx: commands.Context, *, user: Union[discord.Member, discord.User] = None):
         if user is None:
             user = ctx.author
 
@@ -32,29 +32,33 @@ class Economy(commands.Cog):
         bank = data['bank'] if data and data['bank'] is not None else 0
         total = cash + bank
 
-        embed = discord.Embed(title = f"{user.name}'s balance.", description = f"", color = Colors.BASE_COLOR)
-        embed.add_field(name = "Total", value = f"${total}", inline = True)
-        embed.add_field(name = "Cash", value = f"${cash}", inline = True)
-        embed.add_field(name = "Bank", value= f"${bank}", inline = True)
+        embed = discord.Embed(title=f"{user.name}'s balance", description="", color=Colors.BASE_COLOR)
+        embed.add_field(name="Total", value=f"${total:,}", inline=True)
+        embed.add_field(name="Cash", value=f"${cash:,}", inline=True)
+        embed.add_field(name="Bank", value=f"${bank:,}", inline=True)
 
         await ctx.send(embed=embed)
 
-    @command(
-        name = "work",
-        description = "Work to gain money.",
+    @commands.command(
+        name="work",
+        description="Work to gain money.",
     )
-    @cooldown(1, 60, commands.BucketType.user)
+    @commands.cooldown(1, 60, commands.BucketType.user)
     async def work(self, ctx: Context):
         jobs = ["mcdonalds", "kfc", "pizzahut", "secretary", "fire fighter", "police man", "nurse", "coder", "teacher"]
-        randomjob = choice(jobs)
-        data = await self.bot.pool.fetchval("SELECT * FROM economy WHERE user_id = $1", ctx.author.id)
+        randomjob = random.choice(jobs)
+
+       
+        data = await self.bot.pool.fetchrow("SELECT cash FROM economy WHERE user_id = $1", ctx.author.id)
         cash = data['cash'] if data and data['cash'] is not None else 0
+
+        
         earnt = random.randint(10, 250)
         newbal = cash + earnt
 
         await self.bot.pool.execute("UPDATE economy SET cash = $1 WHERE user_id = $2", newbal, ctx.author.id)
 
-        return await ctx.neutral(f"You earnt **${earnt}** working as a **{randomjob}**. Your new balance is **${newbal}**")
+        await ctx.neutral(f"You earned **${earnt}** working as a **{randomjob}**. Your new balance is **${newbal:,}**")
 
 async def setup(bot: Heal):
     return await bot.add_cog(Economy(bot))
