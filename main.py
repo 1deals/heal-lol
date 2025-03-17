230353532621ae3162ed7edcb08309330849b3c1
Harry <162498817+1deals@users.noreply.github.com>
2025-03-17 12:29:27 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ dotenv.load_dotenv()
 
 if __name__ == "__main__":
     bot = Heal()
-    bot.run("MTIzMjc3ODUwODQyNjgwOTM2NQ.GdVCXH.s-fl_dnEm1YYAfDF0RpU87zNTQA2h5jnFpi8dg")
+    bot.run("Your token here")
